233faf47565939d9390bb73ae72a8ed00d420d1d
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-03-19 22:24:24 +0100

    feat[Hand]: add a class for a whole hand
    The Hand class contains all the Cards in the hand, and is able to run checks for hand combinations

---
+++ src/main/java/edu/ntnu/lucyciara/model/Hand.java
@@ -0,0 +1,56 @@
+package edu.ntnu.lucyciara.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Hand {
+  private List<Card> cards;
+
+  // private static final String[] ROYAL_CARDS = new String[] {
+  // "A", "10", "J", "Q", "K"
+  // };
+
+  public Hand(List<Card> cards) {
+    this.cards = cards;
+  }
+
+  // private List<String> getSymbols() {
+  // List<String> symbols = new ArrayList<String>();
+  // for (Card card : this.cards) {
+  // symbols.add(card.getSymbol());
+  // }
+  // return symbols;
+  // }
+
+  public boolean checkFlush() {
+    String suit = this.cards.getFirst().getSuit();
+    return this.cards.stream().allMatch(c -> c.getSuit().equals(suit));
+  }
+
+  // public boolean checkRoyalFlush() {
+  // if (this.checkFlush() &&
+  // this.getSymbols().containsAll(Arrays.asList(ROYAL_CARDS))) {
+  // return true;
+  // } else {
+  // return false;
+  // }
+  // }
+
+  public int sum() {
+    int sum = 0;
+    for (Card card : this.cards) {
+      sum += card.getValue()[0];
+    }
+    return sum;
+  }
+
+  public boolean checkHasCard(String suit, String symbol) {
+    for (Card card : this.cards) {
+      if (card.getSuit().equals(suit) && card.getSymbol().equals(symbol)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
