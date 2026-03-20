7840d18ed2be58aaa6e03ab4e22aafcdfa80d859
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-03-20 13:20:02 +0100

    feat[UI]: add non-funcitonal UI

--- src/main/java/edu/ntnu/lucyciara/App.java
+++ src/main/java/edu/ntnu/lucyciara/App.java
@@ -1,7 +1,23 @@
 package edu.ntnu.lucyciara;
 
-public class App {
+import edu.ntnu.lucyciara.view.MainPage;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    primaryStage.setTitle("Poker-Hand generator");
+    MainPage mainPage = new MainPage();
+    primaryStage.setScene(mainPage.getScene());
+    primaryStage.show();
   }
 }
---
+++ src/main/java/edu/ntnu/lucyciara/view/MainPage.java
@@ -0,0 +1,120 @@
+package edu.ntnu.lucyciara.view;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class MainPage {
+  private Scene scene;
+  private HBox layout;
+  private VBox leftSide;
+  private TextField cards;
+  private VBox cardCheck;
+  private HBox sum;
+  private Label sumText;
+  private TextField sumResult;
+  private HBox cardsOfHearts;
+  private Label cardsOfHeartsText;
+  private TextField cardsOfHeartsResult;
+  private HBox flush;
+  private Label flushText;
+  private TextField flushResult;
+  private HBox queenOfSpades;
+  private Label queenOfSpadesText;
+  private TextField queenOfSpadesResult;
+
+  private VBox rightSide;
+  private Button deal;
+  private Button checkHand;
+
+  private void createSum() {
+    this.sum = new HBox();
+    this.sumText = new Label();
+    this.sumText.setText("The sum of your hand is: ");
+    this.sumResult = new TextField();
+    this.sum.getChildren().add(this.sumText);
+    this.sum.getChildren().add(this.sumResult);
+  }
+
+  private void createCardsOfHeart() {
+    this.cardsOfHearts = new HBox();
+    this.cardsOfHeartsText = new Label();
+    this.cardsOfHeartsText.setText("The cards of hearts in your hand are: ");
+    this.cardsOfHeartsResult = new TextField();
+    this.cardsOfHearts.getChildren().add(this.cardsOfHeartsText);
+    this.cardsOfHearts.getChildren().add(this.cardsOfHeartsResult);
+  }
+
+  private void createFlush() {
+    this.flush = new HBox();
+    this.flushText = new Label();
+    this.flushText.setText("Is your hand a flush? ");
+    this.flushResult = new TextField();
+    this.flush.getChildren().add(flushText);
+    this.flush.getChildren().add(flushResult);
+  }
+
+  private void createQueenOfSpades() {
+    this.queenOfSpades = new HBox();
+    this.queenOfSpadesText = new Label();
+    this.queenOfSpadesText.setText("Does your hand have a queen of spades? ");
+    this.queenOfSpadesResult = new TextField();
+    this.queenOfSpades.getChildren().add(this.queenOfSpadesText);
+    this.queenOfSpades.getChildren().add(this.queenOfSpadesResult);
+  }
+
+  private void createLeftSide() {
+    this.leftSide = new VBox();
+
+    this.cards = new TextField();
+    this.cardCheck = new VBox();
+
+    this.createSum();
+    this.createCardsOfHeart();
+    this.createFlush();
+    this.createQueenOfSpades();
+
+    this.cardCheck.getChildren().add(this.sum);
+    this.cardCheck.getChildren().add(this.cardsOfHearts);
+    this.cardCheck.getChildren().add(this.flush);
+    this.cardCheck.getChildren().add(this.queenOfSpades);
+
+    this.leftSide.getChildren().add(this.cards);
+    this.leftSide.getChildren().add(this.cardCheck);
+  }
+
+  private void createRightSide() {
+    this.rightSide = new VBox();
+    this.deal = new Button();
+    this.deal.setText("Deal");
+    this.checkHand = new Button();
+    this.checkHand.setText("Check Hand");
+
+    this.rightSide.getChildren().add(this.deal);
+    this.rightSide.getChildren().add(this.checkHand);
+  }
+
+  private void createLayout() {
+    this.layout = new HBox();
+    this.createLeftSide();
+    this.createRightSide();
+    this.layout.getChildren().add(this.leftSide);
+    this.layout.getChildren().add(this.rightSide);
+  }
+
+  private void createScene() {
+    this.createLayout();
+    this.scene = new Scene(this.layout, 300, 250);
+  }
+
+  public MainPage() {
+    this.createScene();
+  }
+
+  public Scene getScene() {
+    return this.scene;
+  }
+}
